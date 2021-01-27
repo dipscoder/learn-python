d21c40432eb0c71439c258442d8835b56af6679b
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-27 16:12:15 +0530

    Type Conversion

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-name = input("What is your name? ")
-print("Hello " +name)
+# birth_year = input("What is your birth year? ")
+# age = 2021 - int(birth_year)
+# print(age)
+
+# Calculator
+first_value = input("First: ")
+second_value = input("Second: ")
+sum = float(first_value) + float(second_value)
+print("Sum: "+str(sum))
