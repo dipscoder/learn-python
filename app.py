42e37dab21673740ec20442c4f93469359fb3ec2
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-28 16:11:17 +0530

    List Methods

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 names = ["Dipesh","Jon","Raj","Monica","Phoebe"]
 
-print(names)
-print(names[0])     #Dipesh
-print(names[-1])    #Phoebe
-print(names[-2])    #Monica
-print(names[0:3])   # 0 -> 2
+# names.append("yo")  ['Dipesh', 'Jon', 'Raj', 'Monica', 'Phoebe', 'yo']
+# names.insert(0,"first")   ['first', 'Dipesh', 'Jon', 'Raj', 'Monica', 'Phoebe']
+# names.remove("Jon")     ['Dipesh', 'Raj', 'Monica', 'Phoebe']
+# names.clear()   []
 
+# print("Dipesh" in names)    True
+print(len(names))    
